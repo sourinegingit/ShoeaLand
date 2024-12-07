7fe34a4fd908459cd925e7f43aa7efc9f61aa4ab
negin <neginsouri7979@gmail.com>
2024-12-07 12:53:58 +0430

    create loading page and install react snipper package

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+
+const Loading = () => {
+  return (
+    <div className="flex flex-col items-center gap-36 justify-center  border-2 p-2 bg-gray-200 ">
+      <h1 className="text-3xl font-semibold mt-[15rem]">shoea</h1>
+      <CircleLoader className="mt-40 " />
+    </div>
+  )
+}
+
+export default Loading
