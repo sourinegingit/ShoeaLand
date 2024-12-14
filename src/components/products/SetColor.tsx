3361e89718d5ad0b35c7b5af331eae8d0039a0c9
negin <neginsouri7979@gmail.com>
2024-12-14 12:15:44 +0430

    create type

---
+++ src/components/products/SetColor.tsx
@@ -0,0 +1,18 @@
+
+const SetColor = () => {
+  return (
+    <div>
+      <div className="flex gap-4 items-center">
+        <span className="font-semibold">COLOR</span>
+<div className="h-7 w-7 rounded-full border-teal-300 flex items-center justify-center">
+
+</div>
+<div className="h-5 w-5 rounded-full border-[1.2px] border-slate-300 cursor-pointer">//map on color
+
+</div>
+      </div>
+    </div>
+  )
+}
+
+export default SetColor
---
+++ src/type.ts
@@ -0,0 +1,48 @@
+export interface Category {
+    id: string;
+    title: string;
+    label: string;
+  };
+  
+  export interface ProductImage {
+    title: { src: string };
+    list: { src: string }[];
+  };
+  
+  export interface Product  {
+    id: number;
+    title: string;
+    rate: string;
+    price: string;
+    size: number[];
+    color: string[];
+    brand: string;
+    images: ProductImage;
+    discount: number;
+    views: number;
+    category: string;
+  };
+  
+  export interface Comment  {
+    id: string;
+    text: string;
+    postId: string;
+  };
+  
+  export interface CartItem  {
+    id: number;
+    title: string;
+    price: number;
+    order: number;
+    size: number[];
+    color: string[];
+    brand: string;
+    images: string;
+    sizeselect: number;
+    colorselect: string;
+    quantity: number;
+    totalPr: number;
+  };
+  
+
+  
