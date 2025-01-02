099d61025497eed6a5bd0c9c817bf2611acdb3a2
negin <neginsouri7979@gmail.com>
2025-01-02 09:57:44 +0430

    create baseApi

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const Api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8000/",
 });
 export default Api;
+
+export const BASE_URL="http://localhost:8000"
